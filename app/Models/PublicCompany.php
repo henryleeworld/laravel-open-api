3c941ce1ad12827a457908e08dda89dcb2993525
Henry Lee <henryleeworld@gmail.com>
2025-08-16 00:05:00 +0800

    feat:升級至 laravel 12

--- app/Models/PublicCompany.php
+++ app/Models/PublicCompany.php
@@ -7,22 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class PublicCompany extends Model
 {
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-    ];
-
-    /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
@@ -30,8 +14,8 @@ class PublicCompany extends Model
     protected function casts(): array
     {
         return [
-            'incorporation_date' =>  'datetime:Y-m-d',
-            'listing_date'       =>  'datetime:Y-m-d',
+            'incorporation_date' => 'datetime:Y-m-d',
+            'listing_date'       => 'datetime:Y-m-d',
         ];
     }
 }
